c3cec0520b286a04763cc8d4f508d4269e633c27
Dinesh <dineshronanki777@gmail.com>
2026-06-21 15:16:38 +0530

    Committee section updated

--- src/views/Organising_Committee.vue
+++ src/views/Organising_Committee.vue
@@ -1,74 +1,40 @@
+```vue
 <template>
-  <div class="col-xl-8 col-md-12">
+  <div class="col-xl-12 col-md-12">
     <div class="row mt-2 mb-5 pb-3 mx-2">
       <div class="card card-body mb-5">
 
-        <h2 class="font-weight-bold mt-3">
-          <strong>Organizing Committee</strong>
+        <h2 class="main-title">
+          Organizing Committee
         </h2>
 
-        <div class="content">
-          <p class="article">
+        <div
+          v-for="committee in committees"
+          :key="committee.title"
+          class="committee-section"
+        >
+          <h3 class="section-title">
+            {{ committee.title }}
+          </h3>
 
-            <br /><span class="heading">Chief Patron</span><br />
-            <span>Prof. N. V. Ramana Rao, Director, NIT Raipur</span><br />
+          <div class="committee-grid">
+            <div
+              v-for="member in committee.members"
+              :key="member.name"
+              class="member-card"
+            >
+              <img
+                :src="getImage(member)"
+                :alt="member.name"
+                class="member-image"
+              />
 
-            <br /><span class="heading">Patron(s)</span><br />
-            <span>Prof. Dr. Guru Prasad Subas Chandra Mishra, Dean (R&C), NIT Raipur</span><br />
-            <span>Dr. Geetam Tomar, Director, Govt Engg. College, Sonbhadra, UP </span><br />
-            <span>Shri. M K Badapanda, RRCAT, Indore, Madhya Pradesh</span><br />
-
-            <br /><span class="heading">Honorary Chair</span><br />
-            <span>Prof. Kashem Muttaqi, University of Wollongong, Australia</span><br />
-            <span>Prof. Vivek Agarwal, IIT Bombay</span><br />
-            <span>Prof. Sanjib Panda, NUS Singapore</span><br />
-            <span>Prof. Nikos Hatziargyriou, NTUA, Greece</span><br />
-            <span>Prof. N. P. Padhy, MNIT Jaipur</span><br />
-            <span>Prof. Ashok Kumar Pradhan, IIT Kharagpur</span><br />
-          
-
-            <br /><span class="heading">General Chair / Chairperson</span><br />
-            <span>Prof. Sachin Jain, HOD, Dept. of Electrical Engineering, NIT Raipur</span><br />
-
-            <br /><span class="heading">Coordinators / Organizing Secretaries</span><br />
-            <span>Dr. R. N. Patel, NIT Raipur</span><br />
-            <span>Dr. Monalisa Biswal, NIT Raipur</span><br />
-            <span>Dr. Surajit Sannigrahi, NIT Raipur</span><br />
-            <span>Dr. Saurav Raj, NIT Raipur</span><br />
-
-            <br /><span class="heading">Publication Chairs</span><br />
-            <span>Prof. S. Gupta, NIT Raipur</span><br />
-            <span>Prof. Swapnajit Pattnaik, NIT Raipur</span><br />
-            <span>Prof. S. Ghosh, NIT Raipur</span><br />
-            <span>Dr. Sonti Venu, NIT Raipur</span><br />
-            <span>Dr. Chilaka Ranga, NIT Raipur</span><br />
-
-            <br /><span class="heading">Publicity Chairs</span><br />
-            <span>Prof. Narendra D. Londhe, NIT Raipur</span><br />
-            <span>Prof. Anamika Yadav, NIT Raipur</span><br />
-            <span>Dr. Rajan Kumar, NIT Raipur</span><br />
-            <span>Dr. D. Suresh, NIT Raipur</span><br />
-            <span>Dr. Jitendra Kumar, NIT Jamshedpur</span><br />
-            <span>Dr. Gauri Shankar, IIT(ISM) Dhanbad</span><br />
-            <span>Dr. Ashish Singhal, SGI Bhopal</span><br />
-            <span>Dr. Dipanshu Naware, NIT Trichy</span><br />
-
-            <br /><span class="heading">Finance Chair</span><br />
-            <span>Dr. P. D. Dewangan</span><br />
-            <span>Dr. Ebha Koley, NIT Raipur</span><br />
-            <span>Dr. Ramya Selvaraj, NIT Raipur</span><br />
-
-            <br /><span class="heading">Track Chairs</span><br />
-            <span>Dr. Krishna Murari, IIT Bhilai</span><br />
-            <span>Dr. Shashank Kurmi, IIT Bhilai</span><br />
-            <span>Dr. Shailendra Kumar, IIT Bhilai</span><br />
-            <span>Dr. Om Hari Gupta, NIT Jamshedpur</span><br />
-            <span>Dr. Ravi Bhusan, NIT Jamshedpur</span><br />
-            <span>Dr. Kotsampopoulos Panos, NTUA, Greece</span><br />
-            <span>Dr. J. Ganesh, CEA, New Delhi</span><br />
-            <span>Dr. Saumendra Sarangi, MNNIT Prayagraj</span><br />
-
-          </p>
+              <div class="member-content">
+                <h5>{{ member.name }}</h5>
+                <p>{{ member.organization }}</p>
+              </div>
+            </div>
+          </div>
         </div>
 
       </div>
@@ -79,20 +45,378 @@
 <script>
 export default {
   name: "Organising_Committee",
+
+  data() {
+    return {
+      committees: [
+        {
+          title: "Chief Patron",
+          members: [
+            {
+              name: "Prof. N. V. Ramana Rao",
+              organization: "Director, NIT Raipur"
+            }
+          ]
+        },
+
+        {
+          title: "Patron(s)",
+          members: [
+            {
+              name: "Prof. Dr. Guru Prasad Subas Chandra Mishra",
+              organization: "Dean (R&C), NIT Raipur"
+            },
+            {
+              name: "Dr. Geetam Tomar",
+              organization: "Chair IEEE Madhya Pradesh Section"
+            },
+            {
+              name: "Shri. M K Badapanda",
+              organization: "Chair IEEE PELS & IAS Madhya Pradesh Chapters"
+            },
+            {
+              name: "Prof. Narendra D. Londhe",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Prof. S. Gupta",
+              organization: "NIT Raipur"
+            }
+          ]
+        },
+
+        {
+          title: "Honorary Chair",
+          members: [
+            {
+              name: "Prof. Ashok Kumar Pradhan",
+              organization: "IIT Kharagpur"
+            },
+            {
+              name: "Prof. N. P. Padhy",
+              organization: "MNIT Jaipur"
+            },
+            {
+              name: "Prof. Nikos Hatziargyriou",
+              organization: "NTUA, Greece"
+            },
+            {
+              name: "Prof. Kashem Muttaqi",
+              organization: "University of Wollongong, Australia"
+            },
+            {
+              name: "Prof. Sanjib Panda",
+              organization: "NUS Singapore"
+            },
+            {
+              name: "Prof. Bijay Ketan Panigrahy",
+              organization: "IIT Delhi"
+            },
+            {
+              name: "Prof. Sukumar Mishra",
+              organization: "IIT(ISM) Dhanbad"
+            },
+            {
+              name: "Prof. Bidyadhar Subudhi",
+              organization: "NIT Warangal"
+            }
+          ]
+        },
+
+        {
+          title: "Industrial Chair",
+          members: [
+            {
+              name: "Er. Hemant Verma",
+              organization: "Tripura Electricity Regulatory Commission"
+            }
+          ]
+        },
+
+        {
+          title: "General Chair / Chairperson",
+          members: [
+            {
+              name: "Prof. Sachin Jain",
+              organization: "HOD, Dept. of Electrical Engineering, NIT Raipur"
+            }
+          ]
+        },
+
+        {
+          title: "Coordinators / Organizing Secretaries",
+          members: [
+            {
+              name: "Dr. R. N. Patel",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Monalisa Biswal",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Surajit Sannigrahi",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Saurav Raj",
+              organization: "NIT Raipur"
+            }
+          ]
+        },
+
+        {
+          title: "Publication Chairs",
+          members: [
+            {
+              name: "Prof. Swapnajit Pattnaik",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Prof. S. Ghosh",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Chilaka Ranga",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. B. Shaw",
+              organization: "NIT Raipur"
+            }
+          ]
+        },
+
+        {
+          title: "Publicity Chairs",
+          members: [
+            {
+              name: "Prof. Archana Sharma",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Prof. Anamika Yadav",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Rajan Kumar",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. D. Suresh",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Jitendra Kumar",
+              organization: "NIT Jamshedpur"
+            },
+            {
+              name: "Dr. Gauri Shankar",
+              organization: "IIT(ISM) Dhanbad"
+            },
+            {
+              name: "Dr. Ashish Singhal",
+              organization: "SGI Bhopal"
+            },
+            {
+              name: "Dr. Dipanshu Naware",
+              organization: "NIT Trichy"
+            }
+          ]
+        },
+
+        {
+          title: "Finance Chair",
+          members: [
+            {
+              name: "Dr. P. D. Dewangan",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Varsha Singh",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Ebha Koley",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Ramya Selvaraj",
+              organization: "NIT Raipur"
+            }
+          ]
+        },
+
+        {
+          title: "Track Chairs",
+          members: [
+            {
+              name: "Dr. K. Chandrasekaran",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Hari Priya Vemuganti",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. B. N. Bag",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Sonti Venu",
+              organization: "NIT Raipur"
+            },
+            {
+              name: "Dr. Krishna Murari",
+              organization: "IIT Bhilai"
+            },
+            {
+              name: "Dr. Shashank Kurmi",
+              organization: "IIT Bhilai"
+            },
+            {
+              name: "Dr. Shailendra Kumar",
+              organization: "IIT Bhilai"
+            },
+            {
+              name: "Dr. Om Hari Gupta",
+              organization: "NIT Jamshedpur"
+            },
+            {
+              name: "Dr. Ravi Bhusan",
+              organization: "NIT Jamshedpur"
+            },
+            {
+              name: "Dr. Arpan Hota",
+              organization: "IIT Kharagpur"
+            },
+            {
+              name: "Dr. Satish Belkhode",
+              organization: "IIT Roorkee"
+            },
+            {
+              name: "Dr. Sateesh Kumar Kuncham",
+              organization: "NIT Trichy"
+            },
+            {
+              name: "Dr. Kiran Teeparthi",
+              organization: "NIT AP"
+            },
+            {
+              name: "Dr. Kotsampopoulos Panos",
+              organization: "NTUA, Greece"
+            },
+            {
+              name: "Dr. J. Ganesh",
+              organization: "CEA, New Delhi"
+            },
+            {
+              name: "Dr. Saumendra Sarangi",
+              organization: "MNNIT Prayagraj"
+            }
+          ]
+        }
+      ]
+    };
+  },
+
+  methods: {
+    getImage(member) {
+      try {
+        return require(
+          `@/committee_images/organising_committee/${member.name}, ${member.organization}.png`
+        );
+      } catch (error) {
+        return "";
+      }
+    }
+  }
 };
 </script>
 
 <style scoped>
-.heading {
-  color: #006699;
-  font-size: 16px;
-  font-weight: bold;
-  text-decoration: underline;
-  padding-bottom: 5px;
+.main-title {
+  text-align: center;
+  font-weight: 700;
+  margin-bottom: 40px;
+  color: #1f2937;
+}
+.section-title {
+  font-weight: 700;
+  margin-bottom: 25px;
+  color: #0f172a;
+  text-align: center;
+  padding-left: 0;
+  border-left: none;
+}
+.committee-section {
+  margin-bottom: 50px;
+  text-align: center;
+}
+.committee-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(260px, 300px));
+  gap: 24px;
+  justify-content: center;
 }
 
-.article {
-  line-height: 1.8;
-  font-family: sans-serif;
+.member-card {
+  background: #ffffff;
+  border-radius: 18px;
+  overflow: hidden;
+  box-shadow: 0 8px 25px rgba(0, 0, 0, 0.08);
+  transition: all 0.35s ease;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+}
+
+.member-card:hover {
+  transform: translateY(-8px);
+  box-shadow: 0 16px 35px rgba(0, 0, 0, 0.15);
+}
+
+.member-image {
+  width: 100%;
+  height: 280px;
+  object-fit: contain;
+  object-position: center;
+  background: linear-gradient(135deg, #f8fafc, #e2e8f0);
+  padding: 12px;
+}
+
+.member-content {
+  padding: 18px;
+  text-align: center;
+}
+
+.member-content h5 {
+  font-size: 1rem;
+  font-weight: 700;
+  margin-bottom: 8px;
+  color: #111827;
+  line-height: 1.4;
+}
+
+.member-content p {
+  margin: 0;
+  color: #64748b;
+  font-size: 0.9rem;
+  line-height: 1.5;
+}
+
+@media (max-width: 992px) {
+  .committee-grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (max-width: 576px) {
+  .committee-grid {
+    grid-template-columns: 1fr;
+  }
+
+  .member-image {
+    height: 280px;
+  }
 }
 </style>
+```
--- src/views/Technical_Committee.vue
+++ src/views/Technical_Committee.vue
@@ -1,33 +1,31 @@
+```vue
 <template>
-  <div class="col-xl-8 col-md-12">
+  <div class="col-xl-12 col-md-12">
     <div class="row mt-2 mb-5 pb-3 mx-2">
       <div class="card card-body mb-5">
 
-        <h2 class="font-weight-bold mt-3">
-          <strong>Technical Program Committee (Tentative)</strong>
+        <h2 class="main-title">
+          Technical Program Committee (Tentative)
         </h2>
 
-        <p class="article">
-          <span>Prof. B. Bhattacharyya, IIT (ISM) Dhanbad</span><br />
-          <span>Prof. Parimal Acharjee, NIT Durgapur</span><br />
-          <span>Prof. P. K. Sadhu, IIT (ISM) Dhanbad</span><br />
-          <span>Dr. Rupesh Wandhare, IIT Hyderabad</span><br />
-          <span>Dr. Amit Kumar Jain, IIT Delhi</span><br />
-          <span>Dr. Kalyan Chatterjee, IIT (ISM) Dhanbad</span><br />
-          <span>Dr. Nitika Vats Doohan, SAGE University, Indore</span><br />
-          <span>Dr. Shailendra Kumar Sharma, SGSITS, Indore</span><br />
-          <span>Dr. Rohit Bhakar, MNIT, Jaipur </span><br />
-          <span>Dr. Srinivas Bhaskar Karanki, IIT Bhubaneswar</span><br />
-          <span>Dr. O. H. Gupta, NIT Jamshedpur</span><br />
-          <span>Dr. Papia Ray, VSSUT Burla</span><br />
-          <span>Dr. Ritesh Keshri, VNIT Nagpur</span><br />
-          <span>Dr. A. Kirubakaran, NIT Warangal</span><br />
-          <span>Dr. B. Venugopal Reddy, NIT Warangal</span><br />
-          <span>Dr. Ch. Ramulu, NIT Warangal</span><br />
-          <span>Dr. Nishant Kumar, IIT Gandhinagar</span><br />
-          <span>Dr. Aniruddha Bhattacharya, NIT Durgapur</span><br />
-          <span>Dr. Ram Krishan, NIT Warangal</span><br />
-        </p>
+        <div class="committee-grid">
+          <div
+            v-for="member in members"
+            :key="member.name"
+            class="member-card"
+          >
+            <img
+              :src="getImage(member)"
+              :alt="member.name"
+              class="member-image"
+            />
+
+            <div class="member-content">
+              <h5>{{ member.name }}</h5>
+              <p>{{ member.organization }}</p>
+            </div>
+          </div>
+        </div>
 
       </div>
     </div>
@@ -37,12 +35,176 @@
 <script>
 export default {
   name: "Technical_Committee",
+
+  data() {
+    return {
+      members: [
+        {
+          name: "Prof. B. Bhattacharyya",
+          organization: "IIT (ISM) Dhanbad"
+        },
+        {
+          name: "Prof. Parimal Acharjee",
+          organization: "NIT Durgapur"
+        },
+        {
+          name: "Prof. P. K. Sadhu",
+          organization: "IIT (ISM) Dhanbad"
+        },
+        {
+          name: "Dr. Rupesh Wandhare",
+          organization: "IIT Hyderabad"
+        },
+        {
+          name: "Dr. Amit Kumar Jain",
+          organization: "IIT Delhi"
+        },
+        {
+          name: "Dr. Kalyan Chatterjee",
+          organization: "IIT (ISM) Dhanbad"
+        },
+        {
+          name: "Dr. Nitika Vats Doohan",
+          organization: "SAGE University, Indore"
+        },
+        {
+          name: "Dr. Shailendra Kumar Sharma",
+          organization: "SGSITS, Indore"
+        },
+        {
+          name: "Dr. Rohit Bhakar",
+          organization: "MNIT Jaipur"
+        },
+        {
+          name: "Dr. Srinivas Bhaskar Karanki",
+          organization: "IIT Bhubaneswar"
+        },
+        {
+          name: "Dr. O. H. Gupta",
+          organization: "NIT Jamshedpur"
+        },
+        {
+          name: "Dr. Papia Ray",
+          organization: "VSSUT Burla"
+        },
+        {
+          name: "Dr. Ritesh Keshri",
+          organization: "VNIT Nagpur"
+        },
+        {
+          name: "Dr. A. Kirubakaran",
+          organization: "NIT Warangal"
+        },
+        {
+          name: "Dr. B. Venugopal Reddy",
+          organization: "NIT Warangal"
+        },
+        {
+          name: "Dr. Ch. Ramulu",
+          organization: "NIT Warangal"
+        },
+        {
+          name: "Dr. Nishant Kumar",
+          organization: "IIT Gandhinagar"
+        },
+        {
+          name: "Dr. Aniruddha Bhattacharya",
+          organization: "NIT Durgapur"
+        },
+        {
+          name: "Dr. Ram Krishan",
+          organization: "NIT Warangal"
+        }
+      ]
+    };
+  },
+
+  methods: {
+    getImage(member) {
+      try {
+        return require(
+          `@/committee_images/technical_committee/${member.name}, ${member.organization}.png`
+        );
+      } catch (error) {
+        return "";
+      }
+    }
+  }
 };
 </script>
 
 <style scoped>
-.article {
-  line-height: 1.8;
-  font-family: sans-serif;
+.main-title {
+  text-align: center;
+  font-weight: 700;
+  margin-bottom: 40px;
+  color: #1f2937;
+}
+
+.committee-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+  gap: 24px;
+}
+
+.member-card {
+  background: #ffffff;
+  border-radius: 18px;
+  overflow: hidden;
+  box-shadow: 0 8px 25px rgba(0,0,0,0.08);
+  transition: all 0.35s ease;
+  border: 1px solid rgba(0,0,0,0.05);
+  text-align: center;
+}
+
+.member-card:hover {
+  transform: translateY(-8px);
+  box-shadow: 0 16px 35px rgba(0,0,0,0.15);
+}
+
+.member-image {
+  width: 100%;
+  height: 280px;
+  object-fit: contain;
+  object-position: center;
+  background: linear-gradient(135deg, #f8fafc, #e2e8f0);
+  padding: 12px;
+}
+
+.member-content {
+  padding: 18px;
+  text-align: center;
+}
+
+.member-content h5 {
+  font-size: 1rem;
+  font-weight: 700;
+  margin-bottom: 8px;
+  color: #111827;
+  line-height: 1.4;
+}
+
+.member-content p {
+  margin: 0;
+  color: #64748b;
+  font-size: 0.9rem;
+  line-height: 1.5;
+}
+
+@media (max-width: 992px) {
+  .committee-grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (max-width: 576px) {
+  .committee-grid {
+    grid-template-columns: 1fr;
+  }
+
+  .member-image {
+    height: 280px;
+  }
 }
 </style>
+```
